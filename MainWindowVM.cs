485bc421d572e59e50e0061cc1fbcb325226dfdd
PabloS <pablo.sempere@iesdoctorbalmis.com>
2023-01-09 12:06:13 +0100

    Agregar archivos de proyecto.

---
+++ MainWindowVM.cs
@@ -0,0 +1,90 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperheroesMVVM
+{
+    class MainWindowVM : ObservableObject
+    {
+        private ObservableCollection<Superheroe> heroes;
+
+        private Superheroe heroeActual;
+
+        public Superheroe HeroeActual
+        {
+            get { return heroeActual; }
+            set 
+            {
+                SetProperty(ref heroeActual, value);
+            }
+        }
+
+        private int total;
+
+        public int Total
+        {
+            get { return total; }
+            set 
+            {
+                SetProperty(ref total, value);
+            }
+        }
+
+        private int actual;
+
+        public int Actual
+        {
+            get { return actual; }
+            set 
+            {
+                SetProperty(ref actual, value);
+            }
+        }
+
+        SuperheroeService servicio_heroes = new SuperheroeService();
+
+        public RelayCommand ComandoAvanzar { get; }
+        public RelayCommand ComandoRetroceder { get; }
+
+        public MainWindowVM()
+        {
+            ComandoAvanzar = new RelayCommand(Siguiente);
+            ComandoRetroceder = new RelayCommand(Anterior);
+            heroes = servicio_heroes.ObtenerHeroes();
+            HeroeActual = heroes[0];
+            Total = heroes.Count;
+            Actual = 1;
+        }
+
+        public void Siguiente()
+        { 
+            if (Actual < Total)
+            {
+                Actual++;
+                HeroeActual = heroes[Actual-1];
+            }
+        }
+
+        public void Anterior()
+        {
+            if (Actual > 1)
+            {
+                Actual--;
+                HeroeActual = heroes[Actual-1];
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
